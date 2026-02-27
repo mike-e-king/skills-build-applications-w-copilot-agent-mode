567e02cf8654542bafb9185d24ce0967825861d4
mike-e-king <mike.e.king@gmail.com>
2026-02-27 01:36:43 +0000

    step 3

---
+++ octofit-tracker/backend/octofit_tracker/tests.py
@@ -0,0 +1,28 @@
+# Tests for OctoFit Tracker
+from django.test import TestCase
+from .models import User, Team, Activity, Leaderboard, Workout
+
+class UserModelTest(TestCase):
+    def test_create_user(self):
+        user = User.objects.create(email='test@hero.com', name='Test Hero', team='marvel')
+        self.assertEqual(user.email, 'test@hero.com')
+
+class TeamModelTest(TestCase):
+    def test_create_team(self):
+        team = Team.objects.create(name='marvel', members=['Iron Man', 'Spider-Man'])
+        self.assertEqual(team.name, 'marvel')
+
+class ActivityModelTest(TestCase):
+    def test_create_activity(self):
+        activity = Activity.objects.create(user='Test Hero', type='run', duration=30, date='2026-02-27')
+        self.assertEqual(activity.type, 'run')
+
+class LeaderboardModelTest(TestCase):
+    def test_create_leaderboard(self):
+        lb = Leaderboard.objects.create(team='marvel', points=100)
+        self.assertEqual(lb.team, 'marvel')
+
+class WorkoutModelTest(TestCase):
+    def test_create_workout(self):
+        workout = Workout.objects.create(name='Pushups', description='Do 20 pushups', suggested_for=['marvel'])
+        self.assertEqual(workout.name, 'Pushups')
